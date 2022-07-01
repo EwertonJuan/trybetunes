d2fac3c8bb27038604616d49fe72231826d5a052
Ewerton Juan <juancunha2011@hotmail.com>
2022-07-01 16:33:31 -0300

    Requisito 3: adicionando componente Header [falha em 1 de 8 testes]

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Loading from './Loading';
+import { getUser } from '../services/userAPI';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: {},
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      this.setState({
+        user,
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    const { user, loading } = this.state;
+
+    return (
+      <div data-testid="header-component">
+        {loading ? <Loading /> : <p>{user.name}</p>}
+      </div>
+    );
+  }
+}
+
+export default Header;
