8e8e107566c48326bba03698289bd989e1303cee
Ewerton Juan <juancunha2011@hotmail.com>
2022-07-01 16:13:52 -0300

    Requisito 2: adicionando Login [falha em 1 dos 4 testes]

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      loading: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick = () => {
+    const { username } = this.state;
+    this.setState({ loading: true }, () => {
+      createUser({ name: username });
+      this.setState({ loading: false });
+    });
+  }
+
   render() {
-    return (<div data-testid="page-login" />);
+    const { username, loading } = this.state;
+    const MIN_CHARACTERS = 3;
+
+    return (
+      <div data-testid="page-login">
+        { loading ? <Loading /> : (
+          <>
+            <label htmlFor="login-name-input">
+              Nome
+              {' '}
+              <input
+                type="text"
+                data-testid="login-name-input"
+                placeholder="Insira seu nome"
+                name="username"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <Link to="/search">
+              <button
+                type="submit"
+                onClick={ () => createUser({ name: username }) }
+                data-testid="login-submit-button"
+                disabled={ username.length < MIN_CHARACTERS }
+              >
+                Entrar
+              </button>
+            </Link>
+          </>
+        )}
+      </div>
+    );
   }
 }
 
