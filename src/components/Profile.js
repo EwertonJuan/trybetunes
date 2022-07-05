dc3476230914f69912661876815d94e414ea238e
Ewerton Juan <juancunha2011@hotmail.com>
2022-07-04 22:12:42 -0300

    Requisito 13: adicionando perfil

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      this.setState({ loading: false, user });
+    });
+  }
+
   render() {
+    const { loading, user } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
+        { loading ? <Loading /> : (
+          <div>
+            <img
+              src={ user.image }
+              alt={ `Foto de ${user.name}` }
+              data-testid="profile-image"
+            />
+            <p>{ user.name }</p>
+            <p>{ user.email }</p>
+            <p>{ user.description }</p>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        )}
       </div>
     );
   }
